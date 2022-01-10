3ceaf93ac1201ac29d0cdce0ba4a45cc272fc061
BUYA-GH <64539267+BUYA-GH@users.noreply.github.com>
2022-01-10 16:27:16 +0900

    Delete Server directory

--- Server/account/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.authtoken.models import Token
-from rest_framework import serializers
-
-class TokenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Token
-        fields = ('key', 'user')
--- Server/reviews/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def add_review(request):
-    if request.method == 'POST':
-        print('hello')
